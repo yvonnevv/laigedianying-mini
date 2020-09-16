e8d2a1f42e231b189faea8bcceecb9ace76c1e0f
yvonnexchen <yvonnexchen@tencent.com>
2020-09-16 20:46:43 +0800

    feat: update

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,8 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/recommend/index',
+    'pages/me/index'
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -8,5 +10,19 @@ export default {
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   },
-  cloud: true
+  cloud: true,
+  tabBar: {
+    selectedColor: '#FF505B',
+    color: '#2F2F2F',
+    list: [{
+      pagePath: 'pages/index/index',
+      text: '首页'
+    }, {
+      pagePath: 'pages/recommend/index',
+      text: '推荐'
+    }, {
+      pagePath: 'pages/me/index',
+      text: '我的'
+    }]
+  } 
 }
