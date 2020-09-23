b2f1213746ccc24edd50d2e426f61f70f8bb9cd4
yvonnexchen <yvonnexchen@tencent.com>
2020-09-23 19:06:35 +0800

    feat: add search

--- cloud/functions/douban/config.js
+++ cloud/functions/douban/config.js
@@ -1,6 +1,7 @@
 exports.DOUBAN = {
     list: 'https://movie.douban.com/j/search_subjects',
     info: 'https://movie.douban.com/subject',
+    search: 'https://search.douban.com/movie/subject_search?search_text=',
     movieTags: ['豆瓣高分', '热门', '最新', '经典', '可播放', '冷门佳片', '华语', '欧美', '韩国', '日本', '动作', '喜剧', '爱情', '科幻', '悬疑', '恐怖', '治愈'],
     tvTags: ['热门', '美剧', '英剧', '日剧', '韩剧', '纪录片', '国产剧', '港剧', '日本动画', '综艺'],
     apiKey: '0df993c66c0c636e29ecbb5344252a4a'
--- cloud/functions/douban/index.js
+++ cloud/functions/douban/index.js
@@ -1,4 +1,3 @@
-
 const request = require('async-request');
 const superagent = require('superagent');
 const cheerio = require('cheerio');
@@ -7,6 +6,10 @@ const {
     DOUBAN
 } = require('./config');
 
+const {
+    decrypt
+} = require('./decrypt');
+
 async function getMovieList({
     tag = 0,
     page_start = 0,
@@ -14,11 +17,16 @@ async function getMovieList({
 }) {
     const parseTag = tag ? '高分' : '热门';
     const movies = await request(`${DOUBAN.list}?type=movie&tag=${encodeURIComponent(parseTag)}&page_start=${page_start}&page_limit=${page_limit}`);
-    const { statusCode, body } = movies;
+    const {
+        statusCode,
+        body
+    } = movies;
     if (statusCode === 200 && body) {
         return {
             retcode: 0,
-            result: Object.assign({}, JSON.parse(body), { tag })
+            result: Object.assign({}, JSON.parse(body), {
+                tag
+            })
         }
     } else {
         return {
@@ -41,24 +49,34 @@ function infoParse(infoText) {
     return parseInfoText.trim();
 }
 
-async function getMovieInfo({ id }) {
+async function getMovieInfo({
+    id
+}) {
     const uri = `${DOUBAN.info}/${id}`;
     try {
-        const docs = await superagent
-            .get(uri);
+        const docs = await superagent.get(uri);
         const $ = cheerio.load(docs.text);
+
         const summary = $('#link-report span').text().trim();
         const infosArr = $('#info').text().split('\n');
         const score = $('.rating_wrap .rating_num').text();
         const star = /\d+/.exec($('.bigstar').attr('class'))[0];
         const cover = $('#mainpic img').attr('src');
+        const posterDom = $('.label-trailer .related-pic-video');
         const shortComment = $('#hot-comments .short');
+
+        let poster = '';
+
+        if (posterDom[0]) {
+            poster = /background-image:url\((.*)\)/.exec(posterDom[0].attribs.style)[1];
+        }
+
         let shortText = '';
 
         Array.from(shortComment).forEach(short => {
-          const dataText = short.children[0].data;
-          console.log('dataText', dataText);
-          if (!shortText && dataText.length >= 15 && dataText.length <= 120) shortText = short.children[0].data;
+            const dataText = short.children[0].data;
+            console.log('dataText', dataText);
+            if (!shortText && dataText.length >= 15 && dataText.length <= 120) shortText = short.children[0].data;
         });
 
         let typeText = infosArr.filter(info => /类型/.test(info))[0];
@@ -81,7 +99,8 @@ async function getMovieInfo({ id }) {
                 score,
                 star,
                 cover,
-                shortText
+                shortText,
+                poster
             }
         }
 
@@ -93,13 +112,43 @@ async function getMovieInfo({ id }) {
     }
 }
 
-exports.main = async ({ type, params }) => {
+async function searchMovie({
+    name
+}) {
+    const uri = `${DOUBAN.search}${encodeURIComponent(name)}`;
+
+    try {
+        const docs = await superagent.get(uri);
+        const decryptCtn = /window.__DATA__ = "([^"]+)"/.exec(docs.text);
+        const data = decrypt(decryptCtn[1]) || {
+            playload: {}
+        };
+
+        return {
+            retcode: 0,
+            result: {
+                ...data.playload
+            }
+        }
+    } catch (error) {
+        return {
+            retcode: 1,
+            errmsg: `[search] douban info api error: ${error}`
+        }
+    }
+}
+
+exports.main = async ({
+    type,
+    params
+}) => {
     switch (type) {
         case 'movieList':
             return await getMovieList(params);
         case 'movieInfo':
-            console.log('params', params);
             return await getMovieInfo(params);
+        case 'movieSearch':
+            return await searchMovie(params);
         default:
             return;
     }
